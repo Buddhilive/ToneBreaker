f39dde2d845642715608e060797e6f9dfcbfa0ab
Buddhi Kavindra <visitbudkavin@gmail.com>
2024-05-14 05:31:54 +0530

    use vite to budle the app

--- src/tonebreaker.js
+++ src/tonebreaker.js
@@ -1,5 +1,5 @@
 var seqHeight = 360;
-var dialHeight = window.innerHeight/10;
+var dialHeight = window.innerHeight / 10;
 
 var sequencer = new Nexus.Sequencer('#target', {
     'size': [seqHeight, seqHeight],
@@ -7,42 +7,42 @@ var sequencer = new Nexus.Sequencer('#target', {
     'rows': 32,
     'columns': 16
 });
-sequencer.colorize("accent","#FF6F00");
+sequencer.colorize("accent", "#FF6F00");
 
-var dialer = new Nexus.Dial('#dialer' ,{
-  'size': [dialHeight,dialHeight],
-  'interaction': 'radial',
-  'mode': 'relative', 
-  'min': 0.5,
-  'max': 2,
-  'step': 0.1,
-'value': 1
+var dialer = new Nexus.Dial('#dialer', {
+    'size': [dialHeight, dialHeight],
+    'interaction': 'radial',
+    'mode': 'relative',
+    'min': 0.5,
+    'max': 2,
+    'step': 0.1,
+    'value': 1
 });
-dialer.colorize("accent","#FF6F00");
+dialer.colorize("accent", "#FF6F00");
 var dialNumber = new Nexus.Number('#dialNum');
 dialNumber.link(dialer);
 
-var dialer2 = new Nexus.Dial('#dialer2' ,{
-  'size': [dialHeight,dialHeight],
-  'interaction': 'radial', 
-  'mode': 'relative', 
-  'min': 100,
-  'max': 400,
-  'step': 20,
-'value': 200
+var dialer2 = new Nexus.Dial('#dialer2', {
+    'size': [dialHeight, dialHeight],
+    'interaction': 'radial',
+    'mode': 'relative',
+    'min': 100,
+    'max': 400,
+    'step': 20,
+    'value': 200
 });
-dialer2.colorize("accent","#FFA800");
+dialer2.colorize("accent", "#FFA800");
 var dialNumber2 = new Nexus.Number('#dialNum2');
 dialNumber2.link(dialer2);
 
-var drumSlider = new Nexus.Select('#drumSlider',{
-  'size': [120,30],
-  'options': ['Drumkit1','Drumkit2','Drumkit3']
+var drumSlider = new Nexus.Select('#drumSlider', {
+    'size': [120, 30],
+    'options': ['Drumkit1', 'Drumkit2', 'Drumkit3']
 });
 
-var melodySlider = new Nexus.Select('#melodySlider',{
-  'size': [120,30],
-  'options': ['Melody1','Melody2','Melody3']
+var melodySlider = new Nexus.Select('#melodySlider', {
+    'size': [120, 30],
+    'options': ['Melody1', 'Melody2', 'Melody3']
 });
 
 var playTempo = dialNumber2.value;
@@ -51,10 +51,10 @@ var rnn_temperature = dialNumber.value;
 var melodySample = Melody1;
 var drumSample = Drumkit1;
 
-document.getElementById("btn11").onclick = function() {playSequence()};
-document.getElementById("btn22").onclick = function() {stopSequence()};
-document.getElementById("btn").onclick = function() {musicGenerator()};
-document.getElementById("btnClose").onclick = function() {initializeVar()};
+document.getElementById("btn11").onclick = function () { playSequence() };
+document.getElementById("btn22").onclick = function () { stopSequence() };
+document.getElementById("btn").onclick = function () { musicGenerator() };
+document.getElementById("btnClose").onclick = function () { initializeVar() };
 
 var drum_kick = new Tone.Player("sounds/kick.mp3").toMaster();
 var drum_snare = new Tone.Player("sounds/snare.mp3").toMaster();
@@ -78,18 +78,18 @@ var drum_snare_electro = new Tone.Player("sounds/snare_electro.ogg").toMaster();
 var drum_wood = new Tone.Player("sounds/wood.ogg").toMaster();
 
 var synth = new Tone.Synth({
-  oscillator: {
-    type: 'triangle'
-  },
-  envelope: {
-    attack: 0.001,
-    decay: 0.1,
-    sustain: 0.1,
-    release: 0.1
-  }
+    oscillator: {
+        type: 'triangle'
+    },
+    envelope: {
+        attack: 0.001,
+        decay: 0.1,
+        sustain: 0.1,
+        release: 0.1
+    }
 }).toMaster();
 
-sequencer.on('change', function(v) {
+sequencer.on('change', function (v) {
     //const matMusic = sequencer.matrix.pattern;
     //console.log(matMusic[0][0]);
     //console.log(typeof v.state);
@@ -98,38 +98,38 @@ sequencer.on('change', function(v) {
     }
 });
 
-melodySlider.on('change', function(v) {
+melodySlider.on('change', function (v) {
 
-	switch (v.value) {
-                        case "Melody1":
-                            melodySample = Melody1;
-                            break;
-                        case "Melody2":
-                            melodySample = Melody2;
-                            break;
-                        case "Melody3":
-                            melodySample = Melody3;
-                            break;
-	}
-	
+    switch (v.value) {
+        case "Melody1":
+            melodySample = Melody1;
+            break;
+        case "Melody2":
+            melodySample = Melody2;
+            break;
+        case "Melody3":
+            melodySample = Melody3;
+            break;
+    }
+
 });
 
-drumSlider.on('change', function(v) {
-	switch (v.value) {
-                        case "Drumkit1":
-                            drumSample = Drumkit1;
-                            break;
-                        case "Drumkit2":
-                            drumSample = Drumkit2;
-                            break;
-                        case "Drumkit3":
-                            drumSample = Drumkit3;
-                            break;
-	}
-	
+drumSlider.on('change', function (v) {
+    switch (v.value) {
+        case "Drumkit1":
+            drumSample = Drumkit1;
+            break;
+        case "Drumkit2":
+            drumSample = Drumkit2;
+            break;
+        case "Drumkit3":
+            drumSample = Drumkit3;
+            break;
+    }
+
 });
 
-sequencer.on('step', function(v) {
+sequencer.on('step', function (v) {
     //console.log(v);
     if (v[31] == 1) {
         drum_kick.start();
@@ -235,7 +235,7 @@ var rnnPlayer = new mm.Player();
 function musicGenerator() {
 
     //console.log(rnn_temperature);
-	
+
     if (music_rnn.isInitialized() == true && drum_rnn.isInitialized() == true) {
         sequencer.matrix.populate.all([0, 0]);
 
@@ -245,8 +245,8 @@ function musicGenerator() {
         }
 
         const qns = mm.sequences.quantizeNoteSequence(melodySample, 1);
-		const drums_qns = mm.sequences.quantizeNoteSequence(drumSample, 1);
-		
+        const drums_qns = mm.sequences.quantizeNoteSequence(drumSample, 1);
+
         music_rnn
             .continueSequence(qns, rnn_steps, rnn_temperature)
             .then((sample) => {
@@ -313,9 +313,9 @@ function musicGenerator() {
                     sequencer.matrix.set.cell(counters, rowCell, 1);
                 }
             });
-			
-		
-		drum_rnn
+
+
+        drum_rnn
             .continueSequence(qns, rnn_steps, rnn_temperature)
             .then((sample) => {
 
@@ -382,34 +382,34 @@ function musicGenerator() {
             });
 
     } else {
-		
-		var notification = document.querySelector('.mdl-js-snackbar');
-notification.MaterialSnackbar.showSnackbar(
-  {
-    message: 'Initializing.... Try again shortly...',
-	timeout: 10000
-  }
-);
+
+        var notification = document.querySelector('.mdl-js-snackbar');
+        notification.MaterialSnackbar.showSnackbar(
+            {
+                message: 'Initializing.... Try again shortly...',
+                timeout: 10000
+            }
+        );
 
     }
 };
 
 function playSequence() {
     sequencer.start(playTempo);
-	document.getElementById("btn11").disabled = true;
-	document.getElementById("btn22").disabled =  false;
+    document.getElementById("btn11").disabled = true;
+    document.getElementById("btn22").disabled = false;
 };
 
 function stopSequence() {
     //console.log(v);
     sequencer.stop();
-	document.getElementById("btn22").disabled =  true;
-	document.getElementById("btn11").disabled =  false;
+    document.getElementById("btn22").disabled = true;
+    document.getElementById("btn11").disabled = false;
 };
 
 function initializeVar() {
-playTempo = dialNumber2.value;
-rnn_temperature = dialNumber.value;
+    playTempo = dialNumber2.value;
+    rnn_temperature = dialNumber.value;
 };
 
 var music_rnn = new mm.MusicRNN('https://storage.googleapis.com/magentadata/js/checkpoints/music_rnn/melody_rnn');
