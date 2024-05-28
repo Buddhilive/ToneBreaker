2f2f20468bc6ef60b9c10fd7bffdc443c6815e22
Buddhi Kavindra <visitbudkavin@gmail.com>
2024-05-28 22:00:00 +0530

    play sequance

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,13 +11,34 @@ import * as Tone from "tone";
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'tonebreaker';
+  notes = [
+    { time: "0:0:0", note: "C4", duration: "8n" },
+    { time: "0:0:2", note: "E4", duration: "8n" },
+    { time: "0:1:0", note: "G4", duration: "8n" },
+    { time: "0:1:2", note: "B4", duration: "8n" },
+    { time: "0:2:0", note: "C5", duration: "8n" },
+    { time: "0:3:0", note: "B4", duration: "8n" },
+    { time: "0:3:2", note: "G4", duration: "8n" },
+    { time: "1:0:0", note: "E4", duration: "8n" },
+    { time: "1:0:2", note: "C4", duration: "8n" }
+  ];
 
   runTone() {
     //create a synth and connect it to the main output (your speakers)
     const synth = new Tone.Synth().toDestination();
 
     //play a middle 'C' for the duration of an 8th note
-    synth.triggerAttackRelease("C4", "8n");
+    // synth.triggerAttackRelease("C4", "8n");
+
+    // Create a part that will play the sequence
+    const part = new Tone.Part((time, value) => {
+      synth.triggerAttackRelease(value.note, value.duration, time);
+    }, this.notes).start(0);
+
+    // Set the BPM (beats per minute)
+    //Tone.Transport.bpm.value = 120;
+
+    // Start the transport to hear the sequence
+    //Tone.Transport.start();
   }
 }
